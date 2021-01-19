9a68d1067a62559cc5db87a9dc97d053c7bbc7bf
Leonid Ionov <neonych@gmail.com>
2021-01-19 20:51:09 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+    const openBrackets = [],
+        closingBrackets = [],
+        equalBrackets = [],
+        flags = {}
+
+    bracketsConfig.forEach(item => {
+        if (item[0] === item[1]) {
+            equalBrackets.push(item[0])
+            flags[item[0]] = false
+        } else {
+            openBrackets.push(item[0])
+            closingBrackets.push(item[1])
+        }
+    })
+
+    let copyStr = [...str.split('')],
+
+        answer = copyStr.reduce((stec, item, index, arr) => {
+            if (openBrackets.includes(item)) {
+                stec.push(item)
+                return stec
+            } else if (closingBrackets.includes(item)) {
+                return openBrackets.indexOf(stec.pop()) === closingBrackets.indexOf(item) ? stec : arr.splice(index)
+            } else if (equalBrackets.includes(item) && !flags[item]) {
+                stec.push(item)
+                flags[item] = true
+                return stec
+            } else {
+                flags[item] = false
+                return stec.pop() === item ? stec : arr.splice(index)
+            }
+        }, [])
+    return !answer.length
 }
